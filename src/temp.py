ab069dd7a624cc3906271ef13a43647d3927b178
Anshu Singh <anshu0612>
2019-10-18 15:47:51 +0800

    Rnn

--- src/temp.py
+++ src/temp.py
@@ -55,7 +55,7 @@ batch_size = 64
 #128
 train_samples = 30336
 # 30336
-test_samples = 10000
+test_samples = 10
 #10000
 no_epochs = 100
 
@@ -65,31 +65,47 @@ zeros = ones
 X_t = []
 y_t = []
 print(ones, zeros)
+
 for index, train_label in labels.iterrows():
     label = train_label['label']
+    zero_mat = np.zeros((max_len, 40))
+    data = np.load("data/train/train/" + str(train_label['Id']) + '.npy')
+    zero_mat[:data.shape[0], :] = data
+    X_t.append(zero_mat)
+    y_t.append(label)
+
+x_sampled = []
+y_sampled = []
+X_t = np.array(X_t)
+y_t = np.array(y_t)
+
+from sklearn.utils import shuffle
+X_t, y_t = shuffle(X_t, y_t, random_state=0)
+print("Stage 1", X_t.shape, y_t.shape)
+# shuffle data
+for index in range(y_t.shape[0]):
+    label = y_t[index]
     if label == 0 and zeros > 0:
         zeros = zeros - 1
     if label == 1 and ones > 0:
         ones = ones - 1
     if (zeros == 0 and label == 0) or (ones == 0 and label == 1):
         continue
-    zero_mat = np.zeros((max_len, 40))
-    data = np.load("data/train/train/" + str(train_label['Id']) + '.npy')
-    zero_mat[:data.shape[0], :] = data
     for feature in range(40):
-        average_value = np.nanmean(zero_mat[:, feature])
-        zero_mat[:, feature]= np.nan_to_num(zero_mat[:, feature], nan=average_value)
-    zero_mat = np.delete(zero_mat, [1, 3, 4, 6, 8, 18, 23, 29, 31], axis=1)
-    X_t.append(zero_mat)
-    y_t.append(label)
+        average_value = np.nanmean(X_t[index][:, feature])
+        X_t[index][:, feature] = np.nan_to_num(X_t[index][:, feature], nan=average_value)
+    # zero_mat = np.delete(zero_mat, [1, 3, 4, 6, 8, 18, 23, 29, 31], axis=1)
+    x_sampled.append(X_t[index])
+    y_sampled.append(y_t[index])
 
-X = np.array(X_t)
-y = np.array(y_t)
+X = np.array(x_sampled)
+y = np.array(y_sampled)
+
+print("Stage 2", X.shape, y.shape)
+# print(y)
 # df = pd.read_csv("data/train_kaggle.csv")
 # Y_t = df[:train_samples]
 # y = Y_t.values
-print(X.shape, y.shape)
-# print("FINALLY---->", X[0], y)
 
 X_train, X_val, Y_train, Y_val = train_test_split(X, y, shuffle=True, test_size=0.20)
 
@@ -111,22 +127,19 @@ def generate_data(x_data, y_data, b_size):
             counter = 0
 
 
-data_input = Input(shape=(None, 31))
+data_input = Input(shape=(None, 40))
 
 X = BatchNormalization()(data_input)
 
-sig_conv = Conv1D(40, (1), activation='sigmoid', padding='same')(X)
-rel_conv = Conv1D(40, (1), activation='relu', padding='same')(X)
+sig_conv = Conv1D(64, (1), activation='sigmoid', padding='same')(X)
+rel_conv = Conv1D(64, (1), activation='relu', padding='same')(X)
 X = Multiply()([sig_conv, rel_conv])
 X = Bidirectional(LSTM(64))(X)
-#X = GlobalMaxPooling1D()(X)
+# X = GlobalMaxPooling1D()(X)
 X = Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.0005))(X)
 X = Dropout(0.5)(X)
 X = Dense(1, kernel_regularizer=regularizers.l2(0.0005))(X)
 X = Activation("sigmoid")(X)
-#dense_1 = Dense(16, activation='relu')(lstm)
-#dense_2 = Dense(1)(dense_1)
-#out = Activation('sigmoid')(dense_2)
 model = Model(input=data_input, output=X)
 
 def focal_loss(y_true, y_pred):
@@ -186,7 +199,7 @@ for i in range(0, test_samples):
     for feature in range(40):
         average_value = np.nanmean(zero_mat[:, feature])
         zero_mat[:, feature]= np.nan_to_num(zero_mat[:, feature], nan=average_value)
-    zero_mat = np.delete(zero_mat, [1, 3, 4, 6, 8, 18, 23, 29, 31], axis=1)
+    #zero_mat = np.delete(zero_mat, [1, 3, 4, 6, 8, 18, 23, 29, 31], axis=1)
     X_test.append(zero_mat)
 
 X_test = np.array(X_test)
